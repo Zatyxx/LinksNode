bb096d02b876c47a919a76c0df437c572f3b101d
Zatyxx <andiivan868@gmail.com>
2026-05-04 08:53:53 +0700

    Add the Tooltip

--- src/linkTreeIjat.jsx
+++ src/linkTreeIjat.jsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import './style-ijat.css';
 import FotoProfil from './assets/ijat-mesem.jpeg';
 import LogoReact from './assets/react.svg';
+import {Tooltip} from 'react-tooltip';
+import 'react-tooltip/dist/react-tooltip.css';
 
 function App() {
 
@@ -18,7 +20,7 @@ function App() {
                 <div className="profile">
                     <img src={FotoProfil} alt="ijat-mesem" id="foto-profil" />
                     <div className="profil-desc">
-                        <h1>@Zattxyz</h1>
+                        <h1 data-tooltip-id="tooltip-creator" data-tooltip-content="The Creator">@Zattxyz</h1>
                         <span>Designer | Photographer | Programmer</span>
                     </div>
                 </div>
@@ -35,7 +37,9 @@ function App() {
                     </div>
                 </div>
             </section>
-            <p>Created by Izzat <b>with</b> <img id="react-logo" src={LogoReact} alt="React Logo" /></p>
+            <p>Created by Izzat <b>with</b> <img id="react-logo" src={LogoReact} alt="React Logo" data-tooltip-id="img-tooltip" data-tooltip-content="React" /></p>
+            <Tooltip className="tooltip-style" id="img-tooltip" place="top" variant="light" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", color: "white", backdropFilter: "blur(5px)" }}/>
+            <Tooltip className="tooltip-creator-id" id="tooltip-creator" place="bottom" variant="dark" style={{ backgroundColor: "rgba(0, 0, 0, 0.5)", color: "white", backdropFilter: "blur(5px)" }}/>
         </div>
     );
 }
